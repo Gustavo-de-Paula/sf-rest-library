2b7dfa90e1a21ae2fce641a78e7d5bcc0f2aae3d
Gustavo de Paula <depaula.gustavo@outlook.com>
2023-09-19 16:15:50 -0300

    Added Book service

---
+++ src/main/java/com/gustavo/sfrestlibrary/services/BookService.java
@@ -0,0 +1,17 @@
+package com.gustavo.sfrestlibrary.services;
+
+import com.gustavo.sfrestlibrary.model.BookDTO;
+import com.gustavo.sfrestlibrary.model.BookGenre;
+import org.springframework.data.domain.Page;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface BookService {
+    BookDTO createBook(BookDTO book);
+    Page<BookDTO> listAllBooks(int bookIsbn, String bookName, BookGenre bookgenre, Integer bookVersion, Integer pageNumber, Integer pageSize);
+    Optional<BookDTO> getBookById(UUID bookId);
+    Optional<BookDTO> updateBookById(UUID bookId, BookDTO book);
+    Optional<BookDTO> patchBookById(UUID bookId, BookDTO book);
+    Boolean deleteBookById(UUID bookId);
+}
