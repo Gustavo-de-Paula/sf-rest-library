c5acdcbb8bee4f4e6ce7b16da2bcf40a897c0566
Gustavo de Paula <depaula.gustavo@outlook.com>
2023-09-19 19:32:33 -0300

    Added aplication properties for local MySQL database

--- src/main/java/com/gustavo/sfrestlibrary/SfRestLibraryApplication.java
+++ src/main/java/com/gustavo/sfrestlibrary/SfRestLibraryApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SfRestLibraryApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SfRestLibraryApplication.class, args);
 	}
-
 }
