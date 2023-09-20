b7196e934352c3904eebed9a5da1b6d88f2cf302
Gustavo de Paula <depaula.gustavo@outlook.com>
2023-09-20 19:46:05 -0300

    Changed application properties e first flyway migration

--- src/main/resources/db/migration/V1__init-book-table.sql
+++ src/main/resources/db/migration/V1__init-book-table.sql
@@ -4,9 +4,9 @@ create table book (
     book_id varchar(36) not null,
     book_isbn bigint not null,
     book_name varchar(50) not null,
-    book_genre smallint not null,
+    book_genre tinyint not null,
     book_version integer,
     book_creation_date datetime(6),
     book_update_date datetime(6),
-    primary key (id)
+    primary key (book_id)
 ) engine=InnoDB;
