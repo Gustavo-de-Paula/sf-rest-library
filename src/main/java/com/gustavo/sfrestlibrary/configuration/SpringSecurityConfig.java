2377a37b252d5fedd08d8ec831bde0a3560ee449
Gustavo de Paula <depaula.gustavo@outlook.com>
2023-09-22 20:18:29 -0300

    Update security configurations

--- src/main/java/com/gustavo/sfrestlibrary/configuration/SpringSecurityConfig.java
+++ src/main/java/com/gustavo/sfrestlibrary/configuration/SpringSecurityConfig.java
@@ -3,28 +3,22 @@ package com.gustavo.sfrestlibrary.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
-import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
 @Profile("!test")
 public class SpringSecurityConfig {
     @Bean
-    MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
-        return new MvcRequestMatcher.Builder(introspector);
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, MvcRequestMatcher.Builder mvc) throws Exception {
-        http.authorizeHttpRequests()
-                .requestMatchers(mvc.pattern("/v1/api-docs**"),
-                        mvc.pattern("/swagger-ui/**"),
-                        mvc.pattern("/swagger-ui.html"))
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+                .requestMatchers("/v1/api-docs**", "/swagger-ui/**", "/swagger-ui.html")
                 .permitAll()
                 .anyRequest().authenticated()
-                .and().oauth2ResourceServer().jwt();
+        ).oauth2ResourceServer((oauth2) -> oauth2
+                .jwt(Customizer.withDefaults()));
+
         return http.build();
     }
 }
